3bacb9b1932744781e8d3f0c745d71e9709ae4e1
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-03-02 05:08:27 -0500

    asda

--- GameFiles/TEST/Inventory_v4/control.gd
+++ GameFiles/TEST/Inventory_v4/control.gd
@@ -6,6 +6,14 @@ extends Control
 
 func _ready() -> void:
 	
+	# insert code for removing old slots and adding new ones
+	# this makes the function dynamic in case the number changes
+	#for k in inventory_container.get_children():
+		#k.queue_free()
+	#for l in Global.PLAYER_INVENTORY_TEST:
+		#var new_slot = load("res://Inventory/InvX/inventory_slot.tscn").instantiate()
+		#inventory_container.add_child(new_slot)
+	
 	for j in Global.PLAYER_INVENTORY_TEST:
 		var ic_children = inventory_container.get_children()
 		var new_item: Item = load(Global.PLAYER_INVENTORY_TEST[j][0])
