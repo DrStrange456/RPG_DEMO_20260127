57a892d4302c053f6cda960e8dfbb5d2debbe71f
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-03-02 05:22:20 -0500

    ASDA

--- GameFiles/TEST/Inventory_v4/control.gd
+++ GameFiles/TEST/Inventory_v4/control.gd
@@ -1,19 +1,22 @@
 extends Control
 
-@onready var inventory_slot: InvSlot = $InventoryContainer/InventorySlot
 @onready var inventory_container: InventoryContainer = $InventoryContainer
+@onready var inventory_slot: InvSlot = $InventoryContainer/InventorySlot
 
 
 func _ready() -> void:
 	
 	# insert code for removing old slots and adding new ones
 	# this makes the function dynamic in case the number changes
-	#for k in inventory_container.get_children():
-		#k.queue_free()
-	#for l in Global.PLAYER_INVENTORY_TEST:
-		#var new_slot = load("res://Inventory/InvX/inventory_slot.tscn").instantiate()
-		#inventory_container.add_child(new_slot)
+	for k in inventory_container.get_children():
+		k.queue_free()
+	for l in Global.PLAYER_INVENTORY_TEST:
+		var new_slot = load("res://Inventory/InvX/inventory_slot.tscn").instantiate()
+		new_slot.custom_minimum_size = Vector2(40,40)
+		inventory_container.add_child(new_slot)
 	
+	# NOT WORKING YET
+	inventory_container = $InventoryContainer
 	for j in Global.PLAYER_INVENTORY_TEST:
 		var ic_children = inventory_container.get_children()
 		var new_item: Item = load(Global.PLAYER_INVENTORY_TEST[j][0])
