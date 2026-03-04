57c278981192499462811c1fbf0348fc19e27d54
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-03-04 13:49:07 -0500

    dasda

--- GameFiles/Inventory/InvX/Scripts/storage_handler.gd
+++ GameFiles/Inventory/InvX/Scripts/storage_handler.gd
@@ -10,11 +10,6 @@ func _handle_click_InvToStrg(gcGRID_INV: GridContainer,gcGRID_STRG: GridContaine
 	_remove_from_inventory(gcGRID_INV,intSlotIndex)
 
 
-#func _handle_click(srcSlot: InvSlot,destSlot: InvSlot,cursor_node,is_box_xfer):
-	#var new_item1: Item = load(res1)
-	#var new_item3: Item = load(res3)
-	#srcSlot._update(new_item1, 2)
-	#destSlot._update(new_item3, 3)
 
 func _remove_from_inventory(gcGRID: GridContainer, intSlotIndex: int):
 	# - - Remove from Data then remove from UI
@@ -38,8 +33,23 @@ func _add_to_storage(gcGRID_DEST: GridContainer,item: Item,amount: int):
 			remaining -= to_add
 			if remaining <= 0:
 				return true  # Done adding
-			
-	#print(slot)
+	# Step 2: Fill new empty slots
+	for slot in slots:
+		if slot.itm == null:
+			var to_add = min(item.max_stack, remaining)
+			slot.itm = item
+			slot.label.text = str(int(slot.label.text) + to_add)
+			slot._refresh()
+			remaining -= to_add
+			if remaining <= 0:
+				print(slot)
+				return true  # Done adding
+	# Step 3: Not enough space
+	if remaining > 0:
+		print("Not enough space to add item: %s (Missing %d)" % [item.name, remaining])
+		return false
+	
+		
 	
 	
 	
@@ -48,14 +58,14 @@ func _add_to_storage(gcGRID_DEST: GridContainer,item: Item,amount: int):
 	#var tmp = ptrINVENTORY[intSlotIndex]
 	#var item: Item = load(tmp[0])
 	#slots[4]._update(new_item,tmp[1])
-	
-	
-	
-	# Step 2: Fill new empty slots
-	# Step 3: Not enough space
 
 
 
 
+#func _handle_click(srcSlot: InvSlot,destSlot: InvSlot,cursor_node,is_box_xfer):
+	#var new_item1: Item = load(res1)
+	#var new_item3: Item = load(res3)
+	#srcSlot._update(new_item1, 2)
+	#destSlot._update(new_item3, 3)
 
 # bottom
--- GameFiles/Inventory/InvX/Scripts/test_container.gd
+++ GameFiles/Inventory/InvX/Scripts/test_container.gd
@@ -7,7 +7,7 @@ var res3: String = "res://Inventory/ItemResources/seeds_strawberry.tres"
 
 func _ready() -> void:
 	_set_slot(0,res3,75)
-	_set_slot(1,res2,1)
+	_set_slot(1,res2,97)
 	_set_slot(2,res3,2)
 	_set_slot(3,null,0)
 	_set_slot(4,null,0)
@@ -19,7 +19,6 @@ func _set_slot(indx, itm, qty):
 	var slot_for_update: InvSlot = ic_children[indx]
 	await get_tree().process_frame
 	slot_for_update.itm = load(itm) if itm != null else null
-	print(slot_for_update.itm)
 	slot_for_update._update(new_item, qty)
 	slot_for_update.connect("gui_input", _slot_gui_input.bind(slot_for_update))
 
@@ -32,7 +31,6 @@ func _set_blank_slot(indx):
 func _slot_gui_input(event: InputEvent, _slot: InvSlot):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			print("Left Mouse Button Clicked")
 			var ic_children = get_children()
 			StorageHandler._handle_click(ic_children[3],ic_children[4],self,true)
 		#if event.button_index == MOUSE_BUTTON_RIGHT && event.pressed:
