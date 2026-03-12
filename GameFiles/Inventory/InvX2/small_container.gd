c003cf0510dabc7d1035af253b7f3bbc7617a3f0
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-03-12 05:22:08 -0400

    sdas

--- GameFiles/Inventory/InvX2/small_container.gd
+++ GameFiles/Inventory/InvX2/small_container.gd
@@ -19,8 +19,20 @@ func _load_slots_from_save():
 		inventory[k].set_item(load(Global.STORAGE_TEST[k][0]) if Global.STORAGE_TEST[k][0] else null)
 		inventory[k].set_quantity(Global.STORAGE_TEST[k][1])
 
-
 func bind_inventory(inv):
 	var ui_slots = get_children()
 	for i in ui_slots.size():
 		ui_slots[i].bind_slot(inv[i])
+
+
+func _set_slot(indx):
+	var ic_children = get_children()
+	var slot_for_update: InvSlotUI = ic_children[indx]
+	slot_for_update.connect("gui_input", _slot_gui_input.bind(slot_for_update))
+
+func _slot_gui_input(event: InputEvent, slot: InvSlot):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			StorageHandler.handle_click_StrgToInv(slot,self,get_parent().inventory_container,slot.indx)
+		if event.button_index == MOUSE_BUTTON_RIGHT && event.pressed:
+			StorageHandler.handle_click_StrgToInv_single_item(slot,self,get_parent().inventory_container,slot.indx)
